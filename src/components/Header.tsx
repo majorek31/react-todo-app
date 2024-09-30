c71232f6daee14a34ba8c5ec699035c979fb53ed
majorek31 <m4y0r.offical@gmail.com>
2024-09-30 18:33:56 +0200

    feat: add tasks left info

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,10 +11,12 @@ function countCompletedTasks(todos: Array<Todo>): number {
 export default function Header() {
   const { state } = useTodos();
   const { todos } = state;
+  const completedTasks = countCompletedTasks(todos);
   return (
     <header className="bg-gray-100 p-4 flex items-center justify-between">
       <p className="text-2xl">
-        {countCompletedTasks(todos)}/{todos.length} Tasks
+        <span className="font-bold text-green-500">{completedTasks}</span>/
+        {todos.length} Tasks ({todos.length - completedTasks} tasks left)
       </p>
       <div className="flex items-center justify-between">
         <div className="w-5 h-5 bg-gray-400 rounded-xl"></div>
