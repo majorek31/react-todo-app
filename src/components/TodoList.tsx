3d55df75249ce16ed3146a0699b370c4adf6a3be
majorek31 <m4y0r.offical@gmail.com>
2024-09-30 18:30:47 +0200

    feat: display message if task list is empty

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,9 +6,11 @@ export default function TodoList() {
   const { todos } = state;
   return (
     <div className="lg:w-2/3 sm:w-full lg:h-full flex flex-col pb-8 overflow-y-scroll custom-scrollbar">
-      {todos.map((todo) => (
-        <TodoEntry key={todo.id} todo={todo} />
-      ))}
+      {todos.length ? (
+        todos.map((todo) => <TodoEntry key={todo.id} todo={todo} />)
+      ) : (
+        <h1 className="text-center py-10 text-xl">You have no tasks left</h1>
+      )}
     </div>
   );
 }
