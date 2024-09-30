ce243bdce8121633ce6c4c6b02152504976b1d3c
majorek31 <m4y0r.offical@gmail.com>
2024-09-30 13:44:41 +0200

    feat: wrap AddTaskForm in actual form

--- src/components/AddTaskForm.tsx
+++ src/components/AddTaskForm.tsx
@@ -7,13 +7,20 @@ type AddTaskFormProps = {
 
 export default function AddTask(props: AddTaskFormProps) {
     const [task, setTask] = useState('');
+    function onFormSubmit(e: React.FormEvent<HTMLFormElement>): void {
+        e.preventDefault();
+        props.onTaskAdd({name: task, completed: false});
+        setTask("");
+    }
     return (
         <div className="w-full flex justify-center p-2 border-b-2 pt-6">
             <div className="flex flex-col justify-start items-center w-full">
-                <label className="w-full">
-                    <input type="text" className="border-4 w-full" onChange={(e) => setTask(e.target.value)} value={task}/>
-                </label>
-                <button className="border-2 w-full m-2" onClick={() => {props.onTaskAdd({name: task, completed: false}); setTask("")}}>Add this task</button>
+                <form onSubmit={onFormSubmit}>
+                    <label className="w-full">
+                        <input type="text" className="border-4 w-full" onChange={(e) => setTask(e.target.value)} value={task}/>
+                    </label>
+                    <button className="border-2 w-full m-2">Add this task</button>
+                </form>
             </div>
         </div>
     );
