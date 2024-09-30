7db7f4ee3c7ff9d8a5d3a4b472959e68c6f0af11
majorek31 <m4y0r.offical@gmail.com>
2024-09-30 21:13:01 +0200

    feat: add branding

--- src/components/CenteredBox.tsx
+++ src/components/CenteredBox.tsx
@@ -10,6 +10,11 @@ export default function CenteredBox({ children }: CenteredBoxProps) {
       <div className="bg-white text-black max-lg:w-11/12 md:w-3/6 h-2/3 rounded-2xl shadow-xl overflow-hidden">
         {children}
       </div>
+      <footer className="absolute bottom-0 py-4">
+        <p>
+          made by <a href="https://github.com/majorek31/">majorek31</a>
+        </p>
+      </footer>
     </div>
   );
 }
