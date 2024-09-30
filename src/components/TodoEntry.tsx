43790c906064d5e1f0d686e90ae77215e6cba03d
majorek31 <m4y0r.offical@gmail.com>
2024-09-30 13:39:34 +0200

    fix: change props type name

--- src/components/TodoEntry.tsx
+++ src/components/TodoEntry.tsx
@@ -1,12 +1,12 @@
 import { Todo } from "../lib/Types";
 import TrashCan from '../assets/trash-can.svg';
 
-type ListProps = {
+type TodoEntryProps = {
     todo: Todo;
     onTaskDelete: (todo: Todo) => void;
 };
 
-export default function TodoEntry(props: ListProps) {
+export default function TodoEntry(props: TodoEntryProps) {
     return (
         <div className="w-full flex justify-between p-2 border-b-2 pt-6 hover:bg-gray-200 border-gray-300">
             <p className="pl-4">{props.todo.name}</p>
