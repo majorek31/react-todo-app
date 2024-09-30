84b6649cfa6af64be02dca0e7b2a39048fbc159b
majorek31 <m4y0r.offical@gmail.com>
2024-09-30 17:16:50 +0200

    feat: make completed todos green

--- src/components/TodoEntry.tsx
+++ src/components/TodoEntry.tsx
@@ -8,7 +8,7 @@ type TodoEntryProps = {
 
 export default function TodoEntry(props: TodoEntryProps) {
     return (
-        <div className="w-full flex justify-between p-2 border-b-2 pt-6 hover:bg-gray-200 border-gray-300">
+        <div className={`w-full flex justify-between p-2 border-b-2 pt-6 border-gray-300 ${props.todo.completed ? "bg-green-200" : "" } `} onClick={() => {props.todo.completed = true;}}>
             <p className="pl-4">{props.todo.name}</p>
             <button onClick={() => {props.onTaskDelete(props.todo)}}><img className="w-5" src={TrashCan}/></button>
         </div>
